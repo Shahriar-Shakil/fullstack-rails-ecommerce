e4807f15f2509b9318f68298964364c365023d2b
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-26 01:27:11 +0600

    adding admin layout and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
     root "home#index"
+
+    authenticated :admin_user do
+      root to: "admin#index", as: :admin_root
+    end
+    get "admin" => "admin#index"
 end
